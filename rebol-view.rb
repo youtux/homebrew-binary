4609b9414ee0c8dfd4b4e8ba26f43567716bdbec
Nathaniel Harward <Nathaniel.Harward@searshc.com>
2014-08-15 09:01:38 -0700

    rebol-view 2.7.8.2.5 (new formula)
    Binary formula for Homebrew to install REBOL/View for Intel Macs.
    
    Closes #128.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ rebol-view.rb
@@ -0,0 +1,25 @@
+class RebolView < Formula
+  desc "Rebol distribution with additional graphics support"
+  homepage "http://www.rebol.com/"
+  url "http://www.rebol.com/downloads/v278/rebol-view-278-2-5.tar.gz"
+  sha256 "9aff51eb1d388ec93ce6385eb77a285064a101b5f2f716851170d2e6b9f6e031"
+
+  conflicts_with "rebol-core", :because => "Only one of REBOL Core or View is currently supported"
+
+  depends_on :macos => :snow_leopard
+  depends_on :arch => :intel
+
+  def install
+    bin.install "rebol-view/rebol"
+  end
+
+  def caveats; <<-EOS.undent
+    REBOL license information is available at http://www.rebol.com/license.html
+    or by typing "license" at the REBOL console.
+    EOS
+  end
+
+  test do
+    system "#{bin}/rebol", "--quiet", "--noviewtop", "--do", "print \"Hello, world!\" quit"
+  end
+end
