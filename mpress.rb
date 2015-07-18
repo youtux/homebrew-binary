367cc344e0521ee063d5bd48675bd4ee84606824
Larry Gilbert <larry@l2g.to>
2015-07-18 16:17:00 -0700

    mpress: version test, strip out x86_64, clean up
    * Add test to verify version of executable
    * Install only the i386 slice since x86_64 has problems (see #252)
    * Other changes for `brew audit --strict`
    
    Closes #252.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mpress.rb
+++ mpress.rb
@@ -1,14 +1,27 @@
-require 'formula'
-
 class Mpress < Formula
-  homepage 'http://www.matcode.com/mpress.htm'
-  url 'http://www.matcode.com/mac-mpress.tar.gz'
-  sha1 '1ce0b7f97b081b67abdef6ab1ecb11b1b9296a9e'
-  version '1.21'
+  desc "High-performance packer for Darwin executables"
+  homepage "http://www.matcode.com/mpress.htm"
+  url "http://www.matcode.com/mac-mpress.tar.gz"
+  version "1.21"
+  sha256 "2c3d9e5a20645e1f28e699a8527c3eaa27b8cabe8863be58bb08662ea6f86718"
+  revision 1
 
   depends_on :arch => :intel
 
+  # Install only the i386 slice since x86_64 has problems. See:
+  # https://github.com/Homebrew/homebrew-binary/pull/252
   def install
-    bin.install "mmpress" => "mpress"
+    bin.mkpath
+    system "lipo", "-thin", "i386", "mmpress", "-o", "#{bin}/mpress"
+  end
+
+  def caveats; <<-EOS.undent
+    Because of intractable x86_64 issues, the installed mpress binary is
+    stripped down to i386 code and can only compress other i386 binaries.
+    EOS
+  end
+
+  test do
+    assert_match(/MACHO-MPRESS v#{version}$/, shell_output("#{bin}/mpress", 1))
   end
 end
