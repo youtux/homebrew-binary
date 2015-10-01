a98d9fcb6bde0bd44d278341d3cb8ff52d3e2863
Todd Persen <todd.persen@gmail.com>
2015-09-30 19:56:24 -0700

    chronograf 0.2.0 (new formula)
    Closes #270.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ chronograf.rb
@@ -0,0 +1,52 @@
+class Chronograf < Formula
+  desc "The data visualization tool for InfluxDB"
+  homepage "https://influxdb.com/chronograf/index.html"
+  url "https://s3.amazonaws.com/get.influxdb.org/chronograf/chronograf-darwin_amd64-0.2.0.tar.gz"
+  sha256 "abb41bb7e3c82a9c1468e1187b1b39f5a2b00eca3c8ac0371aab9e07fa34d2fb"
+
+  def install
+    bin.install "chronograf-darwin_amd64-0.2.0" => "chronograf"
+    etc.install "chronograf.toml"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/chronograf</string>
+          <string>-config</string>
+          <string>#{HOMEBREW_PREFIX}/etc/chronograf.toml</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/chronograf.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/chronograf.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    To run Chronograf manually, you can specify the configuration file on the command line:
+      chronograf -config=#{etc}/chronograf.toml
+    EOS
+  end
+
+  test do
+    system "#{bin}/chronograf", "--sample-config"
+  end
+end
