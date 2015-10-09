8d92efeb99814bcc95f0acd38f1828b32e990a0d
Morten Bøgh <morten@justabeech.com>
2015-10-09 10:32:42 +0200

    perforce 2015.1.1240625
    Updated perforce to version 2015.1240625
    
    Just a new version
    
    In the spirit of October #hacktoberfest
    
    Closes #272.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- perforce.rb
+++ perforce.rb
@@ -1,14 +1,14 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2015.1.1227227"
+  version "2015.1.1240625"
 
   if MacOS.prefer_64_bit?
-    url "http://filehost.perforce.com/perforce/r15.1/bin.macosx105x86_64/p4"
-    sha256 "a6d39a35f2be437e2fcbc28a5d0a6b520a584bc122a262c8a5f14cf1dd1811c9"
+    url "http://www.perforce.com/downloads/perforce/r15.1/bin.darwin90x86_64/p4"
+    sha256 "4a1c2258717549e31648648a810708f25f9f48c3d8300279ce649d9f78079494"
   else
-    url "http://filehost.perforce.com/perforce/r15.1/bin.macosx105x86/p4"
-    sha256 "66db5105d11ff34adc8cae73e9b04e5655001b710dbc373f7c6475dba4831acc"
+    url "http://www.perforce.com/downloads/perforce/r15.1/bin.darwin90x86/p4"
+    sha256 "1ed8c05a842b34cc97d822219bc8a8ae5035e0a1b02f7588697d3f1f9271fe04"
   end
 
   def install
