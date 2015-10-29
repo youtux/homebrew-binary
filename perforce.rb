b2f6099bc4a2d74d6d1864a4f23d048b83b7b88e
Morten Bøgh <morten@justabeech.com>
2015-10-29 09:37:43 +0100

    Updated perforce to version 2015.2.1252060
    #Hacktoberfest :)
    
    Closes #273.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- perforce.rb
+++ perforce.rb
@@ -1,14 +1,14 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2015.1.1240625"
+  version "2015.2.1252060"
 
   if MacOS.prefer_64_bit?
-    url "http://www.perforce.com/downloads/perforce/r15.1/bin.darwin90x86_64/p4"
-    sha256 "4a1c2258717549e31648648a810708f25f9f48c3d8300279ce649d9f78079494"
+    url "http://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86_64/p4"
+    sha256 "33162ea34949fb1962c7a74d7e1577b5307f99a7eaed57f86e9b34e90d618e6e"
   else
-    url "http://www.perforce.com/downloads/perforce/r15.1/bin.darwin90x86/p4"
-    sha256 "1ed8c05a842b34cc97d822219bc8a8ae5035e0a1b02f7588697d3f1f9271fe04"
+    url "http://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86/p4"
+    sha256 "2b267843ceef70a8db0d4188b7868eac9b725c2cbb2fd18710de90a7770f3718"
   end
 
   bottle :unneeded
