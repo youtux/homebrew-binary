f704169ce2ec9298c8d0db0defe624ce932cf3d3
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-30 14:32:10 +0100

    jad: modernize; use a sha256

--- jad.rb
+++ jad.rb
@@ -1,16 +1,15 @@
 class Jad < Formula
   desc "Java decompiler"
-  homepage 'http://www.varaneckas.com/jad/'
-  url 'http://www.varaneckas.com/jad/jad158g.mac.intel.zip'
-  sha1 '25bf73676eec21d4cf02f110cb7a06d37cb52f16'
-  version '1.5.8g'
+  homepage "http://www.varaneckas.com/jad/"
+  url "http://www.varaneckas.com/jad/jad158g.mac.intel.zip"
+  version "1.5.8g"
+  sha256 "8e9e4ea6c4177acce6d27325a036f10a72c170ed60e48c37c3483335319d07b9"
 
   bottle :unneeded
 
   def install
-    man1.install 'jad.1'
-    libexec.install 'jad'
+    man1.install "jad.1"
+    libexec.install "jad"
     bin.install_symlink "#{libexec}/jad"
   end
-
 end
