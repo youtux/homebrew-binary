cccf6acd2c80a439e34d655851a6fd9b56fa3906
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-30 14:28:53 +0100

    ddptools: modernize; use a sha256

--- ddptools.rb
+++ ddptools.rb
@@ -1,14 +1,14 @@
 class Ddptools < Formula
   desc "command-line programs to read & write DDP images"
-  homepage 'http://ddp.andreasruge.de'
-  url 'http://ddp.andreasruge.de/dist/ddptools-0.8.8-osx.tar.gz'
-  sha1 '1114f57814f42b88686f2ffb147989906e472524'
+  homepage "http://ddp.andreasruge.de"
+  url "http://ddp.andreasruge.de/dist/ddptools-0.8.8-osx.tar.gz"
+  sha256 "f7be035f59013720ebe975eddfaea600e1970f7d6b04f22733fb46facc065863"
 
   bottle :unneeded
 
   def install
-    bin.install 'ddpinfo', 'cue2ddp', 'cdtinfo'
-    man1.install 'doc/ddpinfo.1', 'doc/cue2ddp.1', 'doc/cdtinfo.1'
+    bin.install "ddpinfo", "cue2ddp", "cdtinfo"
+    man1.install "doc/ddpinfo.1", "doc/cue2ddp.1", "doc/cdtinfo.1"
   end
 
   test do
@@ -16,5 +16,4 @@ class Ddptools < Formula
     system "#{bin}/cue2ddp", "--version"
     system "#{bin}/cdtinfo", "--version"
   end
-
 end
