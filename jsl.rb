b27cf79b49a7d86c50ddd9f861faec7f20a719a7
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-30 14:32:28 +0100

    jsl: modernize; use a sha256

--- jsl.rb
+++ jsl.rb
@@ -1,12 +1,12 @@
 class Jsl < Formula
   desc "JavaScript linter"
-  homepage 'http://www.javascriptlint.com/'
-  url 'http://www.javascriptlint.com/download/jsl-0.3.0-mac.tar.gz'
-  sha1 'a6dd106a05ee81130a27a49d29233afeb8796ab0'
+  homepage "http://www.javascriptlint.com/"
+  url "http://www.javascriptlint.com/download/jsl-0.3.0-mac.tar.gz"
+  sha256 "e7304d03fdb28165370e2859722c4821366e784dd86329bd246231cab610268e"
 
   bottle :unneeded
 
   def install
-    bin.install 'jsl'
+    bin.install "jsl"
   end
 end
