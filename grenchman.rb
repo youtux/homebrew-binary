434bf1fae194780fbf796b55cbf2ae08b1a3acfe
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-30 14:29:50 +0100

    grenchman: audit fix

--- grenchman.rb
+++ grenchman.rb
@@ -4,10 +4,10 @@ class Grenchman < Formula
   url "https://technomancy.github.io/grenchman/downloads/grench-0.2.0-mac"
   sha256 "f70542dffc1f60e372b77582998570b02ff21c85f699fd1854c98e4359804bb6"
 
+  bottle :unneeded
+
   depends_on :arch => :x86_64
   depends_on "libffi"
-
-  bottle :unneeded
 
   def install
     bin.install "grench-0.2.0-mac" => "grench"
