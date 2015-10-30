c1f699e608a581d177f0b0815870ed4cadfbd001
Baptiste Fontaine <batifon@yahoo.fr>
2015-10-30 14:33:27 +0100

    monoberry: modernize; use a sha256

--- monoberry.rb
+++ monoberry.rb
@@ -1,16 +1,16 @@
 class Monoberry < Formula
   desc "Mono for the Blackberry 10 platform"
-  homepage 'http://burningsoda.com/software/monoberry/'
-  url 'https://github.com/roblillack/monoberry/releases/download/0.2.0/monoberry-0.2.0.tgz'
-  sha1 '223c886cc8ed79dd8e21c77760b78f659ede5b8c'
+  homepage "http://burningsoda.com/software/monoberry/"
+  url "https://github.com/roblillack/monoberry/releases/download/0.2.0/monoberry-0.2.0.tgz"
+  sha256 "1a69fe283bc5b658afacbc4133d1f40152c394230eec207759aaaf6af398150d"
 
   bottle :unneeded
 
   def install
-    (share/'monoberry').install "target/lib", "target/target", "target/tool"
-    (bin/'monoberry').write <<-EOS
-#!/bin/sh
-mono #{share}/monoberry/tool/monoberry.exe "$@"
+    (share/"monoberry").install "target/lib", "target/target", "target/tool"
+    (bin/"monoberry").write <<-EOS.undent
+      #!/bin/sh
+      mono #{share}/monoberry/tool/monoberry.exe "$@"
     EOS
   end
 end
