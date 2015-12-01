bfb41e36d71335465a7706a15f9ac3e3dcd8ae42
Baptiste Fontaine <batifon@yahoo.fr>
2015-12-01 17:17:42 +0100

    grads: use sha256s

--- grads.rb
+++ grads.rb
@@ -1,21 +1,21 @@
 class Grads < Formula
   desc "desktop tool to manipule earth science data"
-  homepage 'http://www.iges.org/grads/grads.html'
-  url 'ftp://cola.gmu.edu/grads/2.0/grads-2.0.2-bin-darwin9.8-intel.tar.gz'
-  sha1 '0d42581c614ae627f4b53113c16c0695bd233362'
+  homepage "http://www.iges.org/grads/grads.html"
+  url "ftp://cola.gmu.edu/grads/2.0/grads-2.0.2-bin-darwin9.8-intel.tar.gz"
+  sha256 "da31b5ded94b426acff5b3f27202c74918be71610de424aa0b7877a264eadf59"
 
   # required supplementary files
-  resource 'data2' do
-    url 'ftp://cola.gmu.edu/grads/data2.tar.gz'
-    sha1 'e1cd5f9c4fe8d6ed344a29ee00413aeb6323b7cd'
+  resource "data2" do
+    url "ftp://cola.gmu.edu/grads/data2.tar.gz"
+    sha256 "55419fe15478d8213031bb1bfa7adec96984379373ec751cbe37d4bb22f6ce18"
   end
 
   bottle :unneeded
 
   def install
-    rm 'bin/INSTALL'
-    prefix.install 'bin/COPYRIGHT', 'bin'
-    (lib/'grads').install resource('data2')
+    rm "bin/INSTALL"
+    prefix.install "bin/COPYRIGHT", "bin"
+    (lib/"grads").install resource("data2")
   end
 
   def caveats; <<-EOS.undent
