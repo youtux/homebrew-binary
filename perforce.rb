ac497d90596df7f0e2a6b7119e737c5de2cba1c0
Morten Bøgh <morten@justabeech.com>
2015-12-09 08:56:20 +0100

    perforce 2015.2.1311674
    Updated perforce to version 2015.2.1311674
    
    Closes #279.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- perforce.rb
+++ perforce.rb
@@ -1,14 +1,14 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2015.2.1264740"
+  version "2015.2.1311674"
 
   if MacOS.prefer_64_bit?
     url "https://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86_64/p4"
-    sha256 "e7a1e03b6366ca436f71b3d0a6d47a0f8c707fed8794daba97857d36296e693e"
+    sha256 "361b336973f00ca9bab41a50d4f36bc1807db33d93fdda1649afa5f7130c1e36"
   else
     url "https://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86/p4"
-    sha256 "a55b503fa2aa9db8c91a0693d03a9f4428e9011c296c0ba45eb7d80bde07e59a"
+    sha256 "297d47b77e7993b5010e41196c7d2a94fab7212ce8bb445df3fc1d9e9c75e991"
   end
 
   bottle :unneeded
