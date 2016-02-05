10f50beec8926614019563a154b8845ad6099531
Firat Enderoglu <firat@toytalk.com>
2016-02-04 18:05:52 -0800

    perforce: update SHA
    Closes #292.
    
    Signed-off-by: Alex Dunn <adunn@ucsb.edu>

--- perforce.rb
+++ perforce.rb
@@ -5,10 +5,10 @@ class Perforce < Formula
 
   if MacOS.prefer_64_bit?
     url "https://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86_64/p4"
-    sha256 "0209c2db1f90a5f043015a354d0be2cd1bf1db50855c1a4f3e09d81dcbe46697"
+    sha256 "0276791248bf4f6fcd33f3eafb430b9719b22b0e446769523e57dc831c209098"
   else
     url "https://www.perforce.com/downloads/perforce/r15.2/bin.macosx105x86/p4"
-    sha256 "163cbd80ed67716b7d91b0058e71c2a9fc0755656d003362347fe2dd194c7740"
+    sha256 "9967093883905d712f2a441d7eb815f9c1b193ac51b9cfc1882c0bf179dec2ef"
   end
 
   bottle :unneeded
