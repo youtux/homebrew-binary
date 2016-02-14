a00d85df51acd1e9bcfbb0093b876a0a1139050c
Chris Hoage <iam@chrishoage.com>
2016-02-14 15:12:54 -0800

    kwm 1.1.1 (new formula)
    Closes #290.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

---
+++ kwm.rb
@@ -0,0 +1,52 @@
+class Kwm < Formula
+  desc "Tiling window manager with focus follows mouse for OSX."
+  homepage "https://koekeishiya.github.io/kwm/"
+  url "https://github.com/koekeishiya/kwm/releases/download/v1.1.1/Kwm-1.1.1.zip"
+  sha256 "fef54bc26b1fc7075ec8ea02f973a2a32ad4ad0603b4458b627caea71a3bf146"
+
+  def install
+    bin.install "kwmc"
+    prefix.install %w[kwm kwm-overlay kwmrc]
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_prefix}/kwm</string>
+      </array>
+      <key>KeepAlive</key>
+      <true/>
+      <key>Sockets</key>
+      <dict>
+        <key>Listeners</key>
+        <dict>
+          <key>SockServiceName</key>
+          <string>3020</string>
+          <key>SockType</key>
+          <string>dgram</string>
+          <key>SockFamily</key>
+          <string>IPv4</string>
+      </dict>
+    </dict>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    Copy the example config from #{prefix}/kwmrc into your home directory.
+      mkdir -p ~/.kwm
+      cp #{prefix}/kwmrc ~/.kwm/kwmrc
+    EOS
+  end
+
+  test do
+    system "#{opt_prefix}/kwm", "--version"
+  end
+end
