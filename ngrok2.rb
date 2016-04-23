58305e781eecbf6bd56ace7de1040ca4125d085a
Peter Rhoades <createdbypete@gmail.com>
2016-04-23 13:57:37 +1000

    ngrok2 2.0.25
    Fix broken download urls for Ngrok
    
    - Update version of Ngrok to 2.0.25 (found by running `ngrok -version`)
    
    Closes #316.
    
    Signed-off-by: Alex Dunn <adunn@ucsb.edu>

--- ngrok2.rb
+++ ngrok2.rb
@@ -1,14 +1,14 @@
 class Ngrok2 < Formula
   desc "Secure tunnels to localhost"
   homepage "https://ngrok.com/"
-  version "2.0.19"
+  version "2.0.25"
 
   if MacOS.prefer_64_bit?
-    url "https://dl.ngrok.com/ngrok_#{version}_darwin_amd64.zip"
-    sha256 "ec307920b86778db4c632434cb9fff01ead9edd7438618aab87f5e891791f799"
+    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-#{version}-darwin-amd64.zip"
+    sha256 "62fb342cd1d177415a1b4dba1b40f542e25caa5ea119604981e474a0003c6aa1"
   else
-    url "https://dl.ngrok.com/ngrok_#{version}_darwin_386.zip"
-    sha256 "46bb6437708df9343c1abb3669119870c7878acde2a363bb0f1544a8ddef1f99"
+    url "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-#{version}-darwin-386.zip"
+    sha256 "c7bd3ce01b8a9c9c8c4ee9c14683e96c59bd5d7d6d6f6d1a516c67ca8e7cfe3a"
   end
 
   bottle :unneeded
@@ -18,6 +18,6 @@ class Ngrok2 < Formula
   end
 
   test do
-    system "#{bin}/ngrok"
+    assert_equal "ngrok version #{version}\n", shell_output("#{bin}/ngrok -version")
   end
 end
