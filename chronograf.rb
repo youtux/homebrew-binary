1275c6c83003601d00362243a768a87733d5a2a6
Gunnar <gunnar+github@gunnar.me>
2016-05-13 07:01:23 -0700

    chronograf 0.13.0 (#322)

--- chronograf.rb
+++ chronograf.rb
@@ -1,14 +1,20 @@
 class Chronograf < Formula
   desc "The data visualization tool for InfluxDB"
   homepage "https://influxdata.com/time-series-platform/chronograf/"
-  url "https://s3.amazonaws.com/get.influxdb.org/chronograf/chronograf-0.12.0-darwin_amd64.tar.gz"
-  sha256 "0ad198e5bcc851063459a6184841f8e53fd55b448442d5acb2b9365bd8d1535b"
+  url "https://dl.influxdata.com/chronograf/releases/chronograf-0.13.0-darwin_amd64.tar.gz"
+  sha256 "c1bd95eb31cc9d8e29fb6985c81e8bb8359981c58e5e6f9b9d1ed9905173c5bb"
 
   bottle :unneeded
 
   def install
-    bin.install "chronograf-0.12.0-darwin_amd64" => "chronograf"
+    bin.install "chronograf-0.13.0-darwin_amd64" => "chronograf"
     etc.install "chronograf.toml"
+  end
+
+  def caveats; <<-EOS.undent
+    To run Chronograf manually, you can specify the configuration file on the command line:
+      chronograf -config=#{etc}/chronograf.toml
+    EOS
   end
 
   plist_options :startup => true, :manual => "chronograf"
@@ -41,12 +47,6 @@ class Chronograf < Formula
         <string>#{var}/log/chronograf.log</string>
       </dict>
     </plist>
-    EOS
-  end
-
-  def caveats; <<-EOS.undent
-    To run Chronograf manually, you can specify the configuration file on the command line:
-      chronograf -config=#{etc}/chronograf.toml
     EOS
   end
 
