36df9db7b204e01d8069ec2b93a67b1a24bca203
Morten Bøgh <morten@justabeech.com>
2016-06-07 13:45:42 +0200

    perforce 2016.1.1396108 (#327)

--- perforce.rb
+++ perforce.rb
@@ -1,14 +1,14 @@
 class Perforce < Formula
   desc "Revision control system"
   homepage "http://www.perforce.com/"
-  version "2016.1.1387479"
+  version "2016.1.1396108"
 
   if MacOS.prefer_64_bit?
     url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86_64/p4"
-    sha256 "d54d132bc61bc1db2617cd803e9f915cf7e0b6e8cf76253fa202589b6102dc01"
+    sha256 "a3dd6332c6fe06d6febdaa6f4de35e79e5a9c4deead1441c7574393807bdfff4"
   else
     url "https://www.perforce.com/downloads/perforce/r16.1/bin.macosx105x86/p4"
-    sha256 "45dc664a8ceba2a0fc57dc88527f5194eb1f0781fc19ad864a53ce959efdc678"
+    sha256 "395089e0900915b9c617697446bb600874b6723ce61f496a1281a36896b20980"
   end
 
   bottle :unneeded
